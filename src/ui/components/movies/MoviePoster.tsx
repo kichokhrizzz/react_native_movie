5e0d3f358c794a5b16d47cd1b44f0731ac64fc1c
jhoselbadillocortes <jhosel.badillo.cortes@gmail.com>
2024-03-11 19:20:57 -0600

    add carousel for top rated, upcoming, and popular mvies

--- src/ui/components/movies/MoviePoster.tsx
+++ src/ui/components/movies/MoviePoster.tsx
@@ -19,13 +19,13 @@ export const MoviePoster = ({ movie, height = 420, width = 300 }: Props) => {
             style={({ pressed }) => ({
                 width,
                 height,
-                marginHorizontal: 10,
+                marginHorizontal: 4,
                 paddingBottom: 20,
-                paddingHorizontal: 20,
+                paddingHorizontal: 5,
                 opacity: pressed ? 0.9 : 1
             })
             }>
-            <View style={{ ...styles.imageContainer, width: 300, height: 400 }}>
+            <View style={{ ...styles.imageContainer }}>
                 <Image style={styles.image} source={{ uri: movie.poster }} />
             </View>
         </Pressable>
--- src/ui/screens/home/HomeScreen.tsx
+++ src/ui/screens/home/HomeScreen.tsx
@@ -3,16 +3,28 @@ import { useMovies } from "../../hooks/useMovies"
 import { ScrollView } from "react-native-gesture-handler"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 import PosterCarousel from "../../components/movies/PosterCarousel"
+import HorizontalCarousel from "../../components/movies/HorizontalCarousel"
 const HomeScreen = () => {
     const { top } = useSafeAreaInsets()
-    const { isLoading, nowPlaying } = useMovies()
-    if(isLoading) {
-        return(<Text>Cargando</Text>)
+    const { isLoading, nowPlaying, popular, topRated, upcoming } = useMovies()
+    if (isLoading) {
+        return (<Text>Cargando</Text>)
     }
     return (
         <ScrollView>
             <View style={{ marginTop: top + 20, paddingBottom: 30 }}>
-                <PosterCarousel movies={nowPlaying}/>
+                {/* Carrusel Principal */}
+                <PosterCarousel movies={nowPlaying} />
+
+                {/* Top Rated */}
+                <HorizontalCarousel movies={topRated} title="Top Rated" />
+
+                {/* Populares */}
+                <HorizontalCarousel movies={popular} title="Populares" />
+
+                {/* Proximamente */}
+                <HorizontalCarousel movies={upcoming} title="Próximamente" />
+
             </View>
         </ScrollView>
     )
